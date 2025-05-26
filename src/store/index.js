fc90883bad67390e2b8e163f4354980bc96193ea
Qunainb <waniqunain@gmail.com>
2025-05-26 21:02:55 +0530

    Migrating into Redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,12 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter-slice.js";
+import authReducer from "./auth-slice.js";
 
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterReducer = (state = initialCounterState, action) => {
-  if (action.type === "increment") {
-    return {
-      ...state,
-      counter: state.counter + 1,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      ...state,
-      counter: state.counter - 1,
-    };
-  }
-
-  if (action.type === "increase") {
-    return {
-      ...state,
-      counter: state.counter + action.payload,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      ...state,
-      showCounter: !state.showCounter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
+  },
+});
 
 export default store;
